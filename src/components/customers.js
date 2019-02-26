44a5f0552082962da872354fa706ada0e464c8ea
Merlinda Sumardi <merlindasumardi@MacBook-Pro-Merlinda.local>
2019-02-26 08:37:46 +0700

    fix customer

--- src/components/customers.js
+++ src/components/customers.js
@@ -15,165 +15,57 @@ import {
 } from "native-base";
 
 class Customers extends Component {
+   goToEdit() {
+    this.props.navigation.navigate('AddCustomer', {
+        customerId: 1,
+    });
+   }
+
   render() {
     return (
-      <Container>
-        <Content>
-          <Item
-            rounded
-            style={{
-              marginBottom: 0,
-              marginTop: 10,
-              marginLeft: 10,
-              marginRight: 10
-            }}
-          >
-            <Input placeholder="Search" />
-          </Item>
-          <SwipeRow
-            leftOpenValue={75}
-            rightOpenValue={-75}
-            left={
-              <Button success onPress={() => alert("Add")}>
-                <Icon active name="add" />
-              </Button>
-            }
-            body={
-              <Card>
-                <CardItem header>
-                  <Text>Merlinda</Text>
-                  <Text>082114529397</Text>
-                </CardItem>
-                <CardItem>
-                  <Body>
-                    <Text>Alamat: adfsadfadfafadfafasdfsaf</Text>
-                    <Text>Jumlah barang: 3</Text>
-                    <Text>Total Belanja: Rp 2.000.000</Text>
-                    <Text>DP: Rp 1.000.000</Text>
-                    <Text>Lunas: Belum</Text>
-                  </Body>
-                </CardItem>
-                <CardItem footer>
-                  <Text>Add Order</Text>
-                </CardItem>
-              </Card>
-            }
-            right={
-              <Button danger onPress={() => alert("Trash")}>
-                <Icon active name="trash" />
-              </Button>
-            }
-          />
-          <Card>
-            <CardItem header>
-              <Text>Merlinda</Text>
-              <Text>082114529397</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>Alamat: adfsadfadfafadfafasdfsaf</Text>
-                <Text>Jumlah barang: 3</Text>
-                <Text>Total Belanja: Rp 2.000.000</Text>
-                <Text>DP: Rp 1.000.000</Text>
-                <Text>Lunas: Belum</Text>
-              </Body>
-            </CardItem>
-            <CardItem footer>
-              <Text>Add Order</Text>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem header>
-              <Text>Merlinda</Text>
-              <Text>082114529397</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>Alamat: adfsadfadfafadfafasdfsaf</Text>
-                <Text>Jumlah barang: 3</Text>
-                <Text>Total Belanja: Rp 2.000.000</Text>
-                <Text>DP: Rp 1.000.000</Text>
-                <Text>Lunas: Belum</Text>
-              </Body>
-            </CardItem>
-            <CardItem footer>
-              <Text>Add Order</Text>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem header>
-              <Text>Merlinda</Text>
-              <Text>082114529397</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>Alamat: adfsadfadfafadfafasdfsaf</Text>
-                <Text>Jumlah barang: 3</Text>
-                <Text>Total Belanja: Rp 2.000.000</Text>
-                <Text>DP: Rp 1.000.000</Text>
-                <Text>Lunas: Belum</Text>
-              </Body>
-            </CardItem>
-            <CardItem footer>
-              <Text>Add Order</Text>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem header>
-              <Text>Merlinda</Text>
-              <Text>082114529397</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>Alamat: adfsadfadfafadfafasdfsaf</Text>
-                <Text>Jumlah barang: 3</Text>
-                <Text>Total Belanja: Rp 2.000.000</Text>
-                <Text>DP: Rp 1.000.000</Text>
-                <Text>Lunas: Belum</Text>
-              </Body>
-            </CardItem>
-            <CardItem footer>
-              <Text>Add Order</Text>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem header>
-              <Text>Merlinda</Text>
-              <Text>082114529397</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>Alamat: adfsadfadfafadfafasdfsaf</Text>
-                <Text>Jumlah barang: 3</Text>
-                <Text>Total Belanja: Rp 2.000.000</Text>
-                <Text>DP: Rp 1.000.000</Text>
-                <Text>Lunas: Belum</Text>
-              </Body>
-            </CardItem>
-            <CardItem footer>
-              <Text>Add Order</Text>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem header>
-              <Text>Merlinda</Text>
-              <Text>082114529397</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>Alamat: adfsadfadfafadfafasdfsaf</Text>
-                <Text>Jumlah barang: 3</Text>
-                <Text>Total Belanja: Rp 2.000.000</Text>
-                <Text>DP: Rp 1.000.000</Text>
-                <Text>Lunas: Belum</Text>
-              </Body>
-            </CardItem>
-            <CardItem footer>
-              <Text>Add Order</Text>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
+      <Content>
+        <Item
+          rounded
+          style={{
+            marginBottom: 0,
+            marginTop: 10,
+            marginLeft: 10,
+            marginRight: 10
+          }}
+        >
+          <Input placeholder="Search" />
+        </Item>
+        <SwipeRow
+          leftOpenValue={75}
+          rightOpenValue={-75}
+          left={
+            <Button onPress={() => alert("Add")}>
+              <Icon active name="information-circle" />
+            </Button>
+          }
+          body={
+            <View style={{ marginLeft: 20 }}>
+              <Text>Merlinda - 082114529397</Text>
+              <Text>Alamat: adfsadfadfafadfafasdfsaf</Text>
+              <Text>Jumlah barang: 3</Text>
+              <Text>Total Belanja: Rp 2.000.000</Text>
+              <Text>DP: Rp 1.000.000</Text>
+              <Text>Lunas: Belum</Text>
+            </View>
+          }
+          right={
+              <>
+            <Button warning onPress={() => this.goToEdit()}>
+              <Icon active name="create" />
+            </Button>
+            <Button danger onPress={() => alert("Trash")}>
+              <Icon active name="trash" />
+            </Button>
+            </>
+          }
+        />
+
+      </Content>
     );
   }
 }
